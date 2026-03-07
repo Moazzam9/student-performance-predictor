e1478bd09a08590350f7280afbc461c8a762a6fd
SP22-BCS-010 <moazzamkk13@gmail.com>
2026-03-07 17:38:03 +0500

    Upgrade ML model to Random Forest with new features

--- src/train_model.py
+++ src/train_model.py
@@ -3,13 +3,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import pickle
 
-# Read CSV
+
 data = pd.read_csv("data/students.csv")
 
-# Remove extra spaces in column names
 data.columns = data.columns.str.strip()
 
-# Features
+
 X = data[
     [
         "hours_study", "sleep_hours", "attendance", "previous_score",
@@ -17,17 +16,16 @@ X = data[
     ]
 ]
 
-# Target
+
 y = data["final_score"]
 
-# Split dataset
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train model
 model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
-# Save model
+
 with open("model.pkl", "wb") as f:
     pickle.dump(model, f)
 
