d7ea352e031ed6e4dbb293f602191dfc8d224648
SP22-BCS-010 <moazzamkk13@gmail.com>
2026-03-07 16:56:02 +0500

    Add visualizations in Streamlit app

--- src/train_model.py
+++ src/train_model.py
@@ -3,17 +3,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
 
+
 data = pd.read_csv("data/students.csv")
+
+
+data.columns = data.columns.str.strip()
+
 
 X = data[["hours_study","sleep_hours","attendance","previous_score"]]
 y = data["final_score"]
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = LinearRegression()
-model.fit(X_train,y_train)
+model.fit(X_train, y_train)
 
-with open("model.pkl","wb") as f:
-    pickle.dump(model,f)
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
 
 print("Model trained and saved!")
