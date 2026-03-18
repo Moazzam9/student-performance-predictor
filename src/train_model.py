ca084a4b110304ef3c95c817c77c535c861e0393
SP22-BCS-010 <moazzamkk13@gmail.com>
2026-03-18 17:16:21 +0500

    feat: add ML pipeline with scaling and evaluation metrics

--- src/train_model.py
+++ src/train_model.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import pickle
-
+import numpy as np
 
 data = pd.read_csv("data/students.csv")
-
 data.columns = data.columns.str.strip()
 
 
@@ -15,18 +17,34 @@ X = data[
         "extracurricular", "class_participation", "stress_level"
     ]
 ]
-
-
 y = data["final_score"]
 
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+pipeline = Pipeline([
+    ("scaler", StandardScaler()),
+    ("model", RandomForestRegressor(n_estimators=100, random_state=42))
+])
 
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+pipeline.fit(X_train, y_train)
+
+# Predictions
+y_pred = pipeline.predict(X_test)
+
+
+mae = mean_absolute_error(y_test, y_pred)
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+r2 = r2_score(y_test, y_pred)
+
+print("Model Evaluation:")
+print(f"MAE: {mae:.2f}")
+print(f"RMSE: {rmse:.2f}")
+print(f"R2 Score: {r2:.2f}")
 
 
 with open("model.pkl", "wb") as f:
-    pickle.dump(model, f)
+    pickle.dump(pipeline, f)
 
-print("Model trained and saved with new features!")
+print("Pipeline trained and saved successfully!")
